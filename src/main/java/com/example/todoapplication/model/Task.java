aa5c19efcabc944739150fbf8fbb2572e2737833
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-03 14:21:43 +0530

    chore: create Task model with attributes

---
+++ src/main/java/com/example/todoapplication/model/Task.java
@@ -0,0 +1,27 @@
+package com.example.todoapplication.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+public class Task {
+    private final String id;
+    private String title;
+    private String description;
+    private Status status;      //pending, in_progress, completed
+    private Priority priority;  // low, medium, high
+    private final LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+    public Task(String title, String description, Priority priority) {
+        this.id = UUID.randomUUID().toString();
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.status = Status.PENDING;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
