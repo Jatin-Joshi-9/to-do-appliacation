f9c38ff4bdd4f5d39ab1e39904a0c7df6710fb09
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-04 10:27:06 +0530

    refactor(TaskService): add mock data for testing get tasks api

--- src/main/java/com/example/todoapplication/services/TaskService.java
+++ src/main/java/com/example/todoapplication/services/TaskService.java
@@ -14,18 +14,33 @@ import java.util.Map;
 public class TaskService {
 
     private final Map<Long, Task> tasks = new HashMap<>();
+    private final Map<Long, Task> mockTasks = new HashMap<>();
+
     private Long nextId = 1L;
+    private final boolean useMockData = true; // remove once database is integrated
 
     public List<Task> getFilteredTasks(Status status, Priority priority){
 
-        // Dummy data
-        Task t1 = new Task("test", "test", Priority.LOW);
-        tasks.put(nextId, t1);
+        // Mock data to be removed later
+        Task t1 = new Task("Read Clean code book", "Set a reading time", Priority.LOW);
+        mockTasks.put(nextId, t1);
 
-        return tasks.values().stream()
-                .filter(task -> status == null || task.getStatus() == status)
-                .filter(task -> priority == null || task.getPriority() == priority)
-                .toList();
+        nextId += 1;
+
+        Task t2 = new Task("Review pending Pull request", "", Priority.HIGH);
+        mockTasks.put(nextId, t2);
+
+        if(useMockData){
+            return mockTasks.values().stream()
+                    .filter(task -> status == null || task.getStatus() == status)
+                    .filter(task -> priority == null || task.getPriority() == priority)
+                    .toList();
+        } else {
+            return tasks.values().stream()
+                    .filter(task -> status == null || task.getStatus() == status)
+                    .filter(task -> priority == null || task.getPriority() == priority)
+                    .toList();
+        }
     }
 
 }
