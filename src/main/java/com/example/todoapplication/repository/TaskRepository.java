f51afd710976e08227a829829a4d73daf4008e40
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-04 12:27:00 +0530

    chore(repository): add taskMapByID map in TaskRepository

---
+++ src/main/java/com/example/todoapplication/repository/TaskRepository.java
@@ -0,0 +1,18 @@
+package com.example.todoapplication.repository;
+
+import com.example.todoapplication.model.Task;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class  TaskRepository {
+    private final Map<String, Task> taskMapByID = new HashMap<>();
+
+
+
+}
