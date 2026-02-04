abe73e89e6f5c05da9262175210ae8f793515b3d
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-04 15:43:45 +0530

    feat(controller): add getAll() method in controller

--- src/main/java/com/example/todoapplication/controller/TaskController.java
+++ src/main/java/com/example/todoapplication/controller/TaskController.java
@@ -1,11 +1,15 @@
 package com.example.todoapplication.controller;
 
 import com.example.todoapplication.dto.TaskRequest;
+import com.example.todoapplication.model.Priority;
+import com.example.todoapplication.model.Status;
 import com.example.todoapplication.model.Task;
 import com.example.todoapplication.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/api/tasks")
@@ -21,5 +25,12 @@ public class TaskController {
     @ResponseStatus(HttpStatus.CREATED)
     public Task create(@Valid @RequestBody TaskRequest request) {
         return taskService.create(request);
+    }
+
+    @GetMapping
+    public List<Task> getAll(
+            @RequestParam(required = false) Status status,
+            @RequestParam(required = false) Priority priority) {
+        return taskService.getAllTasks(status, priority);
     }
 }
