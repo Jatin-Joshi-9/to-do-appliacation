cd1800a9ac0ec046e14a130271ef8598b829c55c
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-04 13:39:53 +0530

    fix(service):change the argument of create() method to TaskRequest object

--- src/main/java/com/example/todoapplication/service/TaskService.java
+++ src/main/java/com/example/todoapplication/service/TaskService.java
@@ -1,5 +1,6 @@
 package com.example.todoapplication.service;
 
+import com.example.todoapplication.dto.TaskRequest;
 import com.example.todoapplication.model.Priority;
 import com.example.todoapplication.model.Task;
 import com.example.todoapplication.repository.TaskRepository;
@@ -13,11 +14,19 @@ public class TaskService {
         this.taskRepository = taskRepository;
     }
 
-    public Task create(String title, String description, Priority priority) {
+    public Task create(TaskRequest request) {
+        String title = request.getTitle().trim();
+        title = title.replaceAll("\\s+", " ");
         if (taskRepository.isExistsByTitle(title)) {
-            throw new IllegalArgumentException("Task with title '" + title + "' already exists.");
+            throw new IllegalArgumentException("Task with the same title already exists");
         }
-        Task task = new Task(title, description, priority);
+
+        Task task = new Task( title,
+                request.getDescription().trim(),
+                request.getPriority() != null ? request.getPriority() : Priority.MEDIUM
+        );
+
+
         return taskRepository.save(task);
     }
 }
