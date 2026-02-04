6394999e600cfffc026e37886e8c53c8fbfeb921
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-04 13:44:31 +0530

    chore(dto): add TaskRequest class with attributes title,description,status,priority with validation

---
+++ src/main/java/com/example/todoapplication/dto/TaskRequest.java
@@ -0,0 +1,26 @@
+package com.example.todoapplication.dto;
+
+import com.example.todoapplication.model.Priority;
+import com.example.todoapplication.model.Status;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class TaskRequest {
+    @Size(max = 100, message = "Title max 100 chars")
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @Size(max = 500, message = "Description max 500 chars")
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    private Status status;
+
+    @NotNull(message = "Priority is required")
+    private Priority priority;
+
+    // Getters and Setters
+
+}
