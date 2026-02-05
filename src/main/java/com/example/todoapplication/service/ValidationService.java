5c4613615bd6fe3bfaee3d58858db333d6f719f9
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-06 00:56:31 +0530

    feat: creat isValidDescription() method in ValidationService

--- src/main/java/com/example/todoapplication/service/ValidationService.java
+++ src/main/java/com/example/todoapplication/service/ValidationService.java
@@ -4,4 +4,7 @@ public class ValidationService {
     public static boolean isValidTitle(String title) {
         return title != null && !title.isBlank() && !title.isEmpty() && title.trim().length() <= 100 && title.isEmpty();
     }
+    public static boolean isValidDescription(String description) {
+        return description != null && !description.isBlank() && !description.isEmpty() && description.trim().length() <= 500;
+    }
 }
