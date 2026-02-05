0cff165d5548fe6a8af3767b08beca7f659b625c
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-06 03:09:52 +0530

    refactor(service):add validations calls in TaskService

--- src/main/java/com/example/todoapplication/service/TaskService.java
+++ src/main/java/com/example/todoapplication/service/TaskService.java
@@ -1,12 +1,13 @@
 package com.example.todoapplication.service;
 
 import com.example.todoapplication.dto.TaskRequest;
+import com.example.todoapplication.exception.DuplicateTitleException;
+import com.example.todoapplication.exception.TaskIdNotExistException;
 import com.example.todoapplication.model.Priority;
 import com.example.todoapplication.model.Status;
 import com.example.todoapplication.model.Task;
 import com.example.todoapplication.repository.TaskRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,76 +19,83 @@ public class TaskService {
         this.taskRepository = taskRepository;
     }
 
+    public boolean isTitleExist(String title) {
+        return taskRepository.isExistsByTitle(title);
+    }
+
     public Task create(TaskRequest request) {
         String title = request.getTitle().trim();
         title = title.replaceAll("\\s+", " ");
-        if (taskRepository.isExistsByTitle(title)) {
-            throw new IllegalArgumentException("Task with the same title already exists");
+        if (isTitleExist(title)) {
+            throw new DuplicateTitleException("Task with the " + title + " title already exists");
         }
 
-        Task task = new Task( title,
+        Task task = new Task(title,
                 request.getDescription().trim(),
-                request.getPriority() != null ? request.getPriority() : Priority.MEDIUM
-        );
-
+                request.getPriority() != null ? request.getPriority() : Priority.MEDIUM);
 
         return taskRepository.save(task);
     }
+
     public List<Task> getAllTasks(Status status, Priority priority) {
         return taskRepository.findAll().stream()
                 .filter(task -> status == null || task.getStatus() == status)
                 .filter(task -> priority == null || task.getPriority() == priority)
                 .collect(Collectors.toList());
     }
+
     public Task getTaskById(String id) {
         Task task = taskRepository.findById(id);
         if (task == null) {
-            throw new IllegalArgumentException("Task not found with id: " + id);
+            throw new TaskIdNotExistException("Task not found with id: " + id);
         }
         return task;
     }
-    
+
     public Task update(String id, TaskRequest request) {
         Task existingTask = getTaskById(id);
         boolean updated = false;
 
-        if (request.getTitle() != null && !request.getTitle().isBlank() && !request.getTitle().isEmpty() && request.getTitle().trim().length() <= 100) {
-            String newTitle = request.getTitle().trim().replaceAll("\\s+", " ");
-            if (!existingTask.getTitle().equalsIgnoreCase(newTitle)) { 
-                if (taskRepository.isExistsByTitle(newTitle)) {
-                    throw new IllegalArgumentException("Task with the same title already exists");
-                }
-                existingTask.setTitle(newTitle);
-                updated = true;
-            }
-        }
-
-        if (request.getDescription() != null && request.getDescription().trim().length() <= 500 && !request.getDescription().isBlank() && !request.getDescription().isEmpty()) {
+        if(ValidationService.isValidDescription(request.getDescription())) {
             String newDesc = request.getDescription().trim();
-            if (!newDesc.equalsIgnoreCase(existingTask.getDescription())) { 
+            if(!newDesc.equalsIgnoreCase(existingTask.getDescription())) {
                 existingTask.setDescription(newDesc);
                 updated = true;
             }
         }
 
-        if (request.getPriority() != null && !request.getPriority().equals(existingTask.getPriority()) ) {
+        if(ValidationService.isValidPriority(request.getPriority())
+                && !request.getPriority().equals(existingTask.getPriority())) {
             existingTask.setPriority(request.getPriority());
             updated = true;
         }
 
-        if (request.getStatus() != null && !request.getStatus().equals(existingTask.getStatus())) {
+        if(ValidationService.isValidStatus(request.getStatus())
+                && !request.getStatus().equals(existingTask.getStatus())) {
             existingTask.setStatus(request.getStatus());
             updated = true;
         }
 
-        if (updated) {
+        if(ValidationService.isValidTitle(request.getTitle())) {
+            String newTitle = request.getTitle().trim().replaceAll("\\s+", " ");
+
+            if(!newTitle.equalsIgnoreCase(existingTask.getTitle())&&isTitleExist(newTitle)) {
+
+                throw new DuplicateTitleException(
+                        "Task with the " + newTitle + " title already exists");
+            }
+
+            if(!newTitle.equalsIgnoreCase(existingTask.getTitle())) {
+                existingTask.setTitle(newTitle);
+                updated = true;
+            }
+        }
+
+        if(updated) {
             existingTask.setUpdatedAt();
             return taskRepository.save(existingTask);
         }
-        else{
-            throw new IllegalArgumentException("Already up to date fields provided");
-        }
-         
+        return existingTask;
     }
 
 }
--- src/main/java/com/example/todoapplication/service/ValidationService.java
+++ src/main/java/com/example/todoapplication/service/ValidationService.java
@@ -6,16 +6,22 @@ import com.example.todoapplication.model.Status;
 
 public class ValidationService {
     public static boolean isValidTitle(String title) {
-        return title != null && !title.isBlank() && !title.isEmpty() && title.trim().length() <= 100 && title.isEmpty();
+        return title != null && !title.isBlank() && !title.isEmpty() && title.trim().length() <= 100;
     }
     public static boolean isValidDescription(String description) {
-        return description != null && !description.isBlank() && !description.isEmpty() && description.trim().length() <= 500;
+        return description != null && !description.isBlank()  && description.trim().length() <= 500;
     }
     public static boolean isValidStatus(Status status) {
-        return status == null || status.equals(Status.PENDING) || status.equals(Status.IN_PROGRESS) || status.equals(Status.COMPLETED);
+        if(status == null) {
+            return false;
+        }
+        return status.equals(Status.PENDING) || status.equals(Status.IN_PROGRESS) || status.equals(Status.COMPLETED);
     }
     public static boolean isValidPriority(Priority priority) {
-        return priority == null || priority.equals(Priority.LOW) || priority.equals(Priority.MEDIUM) || priority.equals(Priority.HIGH);
+        if(priority == null) {
+            return false;
+        }
+        return priority.equals(Priority.LOW) || priority.equals(Priority.MEDIUM) || priority.equals(Priority.HIGH);
     }
 
 }
