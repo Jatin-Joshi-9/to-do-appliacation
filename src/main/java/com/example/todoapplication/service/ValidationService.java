27d66d21604f3b7160a9d6de1086155814f02a9f
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-06 00:58:27 +0530

    feat: create isValidStatus() method in ValidationService

--- src/main/java/com/example/todoapplication/service/ValidationService.java
+++ src/main/java/com/example/todoapplication/service/ValidationService.java
@@ -1,5 +1,7 @@
 package com.example.todoapplication.service;
 
+import com.example.todoapplication.model.Status;
+
 public class ValidationService {
     public static boolean isValidTitle(String title) {
         return title != null && !title.isBlank() && !title.isEmpty() && title.trim().length() <= 100 && title.isEmpty();
@@ -7,4 +9,8 @@ public class ValidationService {
     public static boolean isValidDescription(String description) {
         return description != null && !description.isBlank() && !description.isEmpty() && description.trim().length() <= 500;
     }
+    public static boolean isValidStatus(Status status) {
+        return status == null || status.equals(Status.PENDING) || status.equals(Status.IN_PROGRESS) || status.equals(Status.COMPLETED);
+    }
+    
 }
