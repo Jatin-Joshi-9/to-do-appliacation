ba06a3e450bf5abba5aec71b048c12d3be783e2f
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-06 10:12:43 +0530

    fix: udate isValidTitle with removal of title.isEmpty() check

--- src/main/java/com/example/todoapplication/service/ValidationService.java
+++ src/main/java/com/example/todoapplication/service/ValidationService.java
@@ -6,7 +6,7 @@ import com.example.todoapplication.model.Status;
 
 public class ValidationService {
     public static boolean isValidTitle(String title) {
-        return title != null && !title.isBlank() && !title.isEmpty() && title.trim().length() <= 100;
+        return title != null && !title.isBlank()  && title.trim().length() <= 100;
     }
     public static boolean isValidDescription(String description) {
         return description != null && !description.isBlank()  && description.trim().length() <= 500;
